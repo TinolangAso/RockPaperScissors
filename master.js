82092c45c79d266b278357fa09746a2f86b2a0d9
TinolangAso <94122834+TinolangAso@users.noreply.github.com>
2024-06-18 08:45:58 +0800

    Added a getHumanChoice() function that expects the user for a string input and outputs that input(Still checking if it works properly) also added the HumanChoice as the return.

--- master.js
+++ master.js
@@ -12,6 +12,17 @@ function main()
         let n = getComputerChoice(); //To index the ComputerChoice
 
         console.log(`Computer choice: ${ComputerChoice[n]}\n`);
+
+        let HumanChoice;
+
+        function getHumanChoice()
+        {
+                return prompt("What's your pick? ");
+        }
+
+        HumanChoice = getHumanChoice();
+
+        console.log(`Human choice: ${HumanChoice}`);
 }
 
 main();
