30ac369defec0191a7f98e4324d2d9e6dbf0ba5e
TinolangAso <94122834+TinolangAso@users.noreply.github.com>
2024-06-18 08:34:10 +0800

    Implemented a getComputerChoice() function that doesn't take any input but returns a random number from 0 to 2.

--- master.js
+++ master.js
@@ -2,6 +2,15 @@
 
 function main()
 {
+        let ComputerChoice;
+
+        function getComputerChoice()
+        {
+                return Math.floor(Math.random() * 3);
+        }
+
+        ComputerChoice = getComputerChoice();
+        console.log(`Computer choice: ${ComputerChoice}\n`);
 }
 
 main();
