016cc9fb945fd134fa0e0f60568f5ad37a4eadc2
TinolangAso <94122834+TinolangAso@users.noreply.github.com>
2024-06-18 08:39:30 +0800

    Modified the code, by making the ComputerChoice an array of strings instead, so that the getComputerChoice returns the value as an index of the ComputerChoice.

--- master.js
+++ master.js
@@ -2,15 +2,16 @@
 
 function main()
 {
-        let ComputerChoice;
+        let ComputerChoice = ["Rock", "Paper", "Scissors"];
 
         function getComputerChoice()
         {
                 return Math.floor(Math.random() * 3);
         }
 
-        ComputerChoice = getComputerChoice();
-        console.log(`Computer choice: ${ComputerChoice}\n`);
+        let n = getComputerChoice(); //To index the ComputerChoice
+
+        console.log(`Computer choice: ${ComputerChoice[n]}\n`);
 }
 
 main();
