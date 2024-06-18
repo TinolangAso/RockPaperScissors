3a3fea39eabde11d31b59569168c7afa177a8f84
TinolangAso <94122834+TinolangAso@users.noreply.github.com>
2024-06-18 14:05:16 +0800

    Finally finished the project. Although there can be improvements, such as user input validation. And also separating game logic from interaction logic

--- master.js
+++ master.js
@@ -3,59 +3,81 @@
 function main()
 {
         let computerChoices = ["rock", "paper", "scissor"];
-
-        function getComputerChoice()
-        {
-                let randomIndex = Math.floor(Math.random() * 3);
-                return computerChoices[randomIndex];
-        }
-
-        let computerChoice = getComputerChoice();
-        console.log(`computer choice: ${computerChoice}\n`);
-
-
-        let humanChoice;
-
-        function getHumanChoice()
-        {
-                let choice = prompt("What's your pick? ");
-                return choice.trim().toLowerCase();
-        }
-
-        humanChoice = getHumanChoice();
-        console.log(`human choice: ${humanChoice}\n`);
-
         let humanScore = 0;
         let computerScore = 0;
 
-        function playRound(humanChoice, computerChoice)
+
+
+
+        function playGame()
         {
-                
-                if (humanChoice === computerChoice)
+                        for(let i = 0; i < 5; i++)
+                        {
+                                function getComputerChoice()
+                                {
+                                        let randomIndex = Math.floor(Math.random() * 3);
+                                        return computerChoices[randomIndex];
+                                }
+
+                                let computerChoice = getComputerChoice();
+
+
+                                let humanChoice;
+
+                                function getHumanChoice()
+                                {
+                                        let choice = prompt("What's your pick? ");
+                                        return choice.trim().toLowerCase();
+                                }
+
+                                humanChoice = getHumanChoice();
+
+                                function playRound(humanChoice, computerChoice)
+                                {
+                                        
+                                        if (humanChoice === computerChoice)
+                                        {
+                                                console.log(`It's a tie!\n`);
+                                        }
+
+
+                                        else if (
+                                                (humanChoice === "rock" && computerChoice === "scissor")||
+                                                (humanChoice === "paper" && computerChoice === "rock")||
+                                                (humanChoice === "scissor" && computerChoice === "paper")
+                                            )
+                                        {
+                                                humanScore++;
+                                                console.log(`You win! ${humanChoice} beats ${computerChoice}\n`);
+                                        }
+
+                                        else
+                                        {
+                                                computerScore++;
+                                                console.log(`You lose! ${humanChoice} loses against ${computerChoice}\n`);
+                                        }
+
+                                }
+                                    playRound(humanChoice, computerChoice);
+                        }
+            if(humanScore > computerScore)
                 {
-                        console.log(`It's a tie!\n`);
+                        console.log(`\nYou win! up by ${humanScore - computerScore}\n`);
                 }
 
-
-                else if (
-                        (humanChoice === "rock" && computerChoice === "scissor")||
-                        (humanChoice === "paper" && computerChoice === "rock")||
-                        (humanChoice === "scissor" && computerChoice === "paper")
-                    )
+            else if (computerScore < humanScore)
                 {
-                        humanScore++;
-                        console.log(`You win! ${humanChoice} beats ${computerChoice}\n`);
+                        console.log(`\nYou lose! by ${computerScore - humanScore}\n`);
                 }
 
-                else
+            else
                 {
-                        computerScore++;
-                        console.log(`You lose! ${humanChoice} loses against ${computerChoice}\n`);
+                        console.log("\nCongratulations! It's a tie!\n");
                 }
 
         }
 
-        playRound(humanChoice, computerChoice);
+        playGame();
 }
 
 main();
